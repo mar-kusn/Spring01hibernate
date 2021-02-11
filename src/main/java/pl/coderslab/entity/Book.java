3be596d1569487ca7e8de26b8336378262de47b1
Mariusz <mar-kus@o2.pl>
2021-02-11 22:57:09 +0100

    Book entity added, persistance.xml for generating table: drop and create

---
+++ src/main/java/pl/coderslab/entity/Book.java
@@ -0,0 +1,63 @@
+package pl.coderslab.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String title;
+    private int rating;
+    private String description;
+
+    public Book() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Book setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Book setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public Book setRating(int rating) {
+        this.rating = rating;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Book setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", rating=" + rating +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
