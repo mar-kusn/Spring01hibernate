d687d856dfa82840ff99da531c5e9939252b4f66
Mariusz <mar-kus@o2.pl>
2021-02-14 16:30:43 +0100

    add method declarations

---
+++ src/main/java/pl/coderslab/ut/tddz2/Factors.java
@@ -0,0 +1,13 @@
+package pl.coderslab.ut.tddz2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Factors {
+    public static List<Integer> generatePrimeFactors(int n) {
+        List<Integer> result = new ArrayList<>();
+
+
+        return result;
+    }
+}
